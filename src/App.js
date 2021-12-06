d8cd0ce79d48169c9a054b6d27ffb06918e4c6c8
Chetan <chetanoli2012@gmail.com>
2021-12-06 20:58:14 +0530

    first commmit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { BigSuccessButton, DangerButton } from "./partiallyApply";
+
+function App() {
+  return (
+    <>
+      <DangerButton text="This is danger action!" />
+      <BigSuccessButton text="Operation completed successfully!" />
+    </>
+  );
+}
+
+export default App;
